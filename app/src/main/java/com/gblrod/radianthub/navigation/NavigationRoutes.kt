435a7a66ec89c26f81764a03af569459b09abb51
Gabriel Rodrigues <114709637+gblrodrigues@users.noreply.github.com>
2026-08-10 20:47:19 -0300

    fix: improve bottom bar and drawer navigation (#34)

---
+++ app/src/main/java/com/gblrod/radianthub/navigation/NavigationRoutes.kt
@@ -0,0 +1,8 @@
+package com.gblrod.radianthub.navigation
+
+val bottomBarRoutes = setOf(
+    Routes.Home.ROUTE,
+    Routes.Agents.ROUTE_WITH_ARGUMENT,
+    Routes.Maps.ROUTE_WITH_ARGUMENT,
+    Routes.Favorites.ROUTE
+)
