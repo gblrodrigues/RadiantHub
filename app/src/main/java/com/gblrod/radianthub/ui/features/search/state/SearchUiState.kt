3d1b6352a2fc4621481b032a6c0c63a6abd2b085
Gabriel <114709637+gblrodrigues@users.noreply.github.com>
2026-08-19 04:53:49 -0300

    refactor: improve search state and UX

--- app/src/main/java/com/gblrod/radianthub/ui/features/search/state/SearchUiState.kt
+++ app/src/main/java/com/gblrod/radianthub/ui/features/search/state/SearchUiState.kt
@@ -2,16 +2,19 @@ package com.gblrod.radianthub.ui.features.search.state
 
 import com.gblrod.radianthub.ui.features.search.model.SearchItem
 
-sealed class SearchUiState {
-    object Loading : SearchUiState()
+sealed interface SearchUiState {
+    data class Loading(
+        val query: String = ""
+    ) : SearchUiState
 
     data class Success(
         val query: String,
         val results: List<SearchItem>
-    ) : SearchUiState()
+    ) : SearchUiState
 
     data class Error(
+        val query: String,
         val messageResId: Int,
         val code: Int? = null
-    ) : SearchUiState()
+    ) : SearchUiState
 }
